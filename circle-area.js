45185596ee23b00241c3ff726b407ccbea94e620
ajaymandakki <ajaymandakki@gmail.com>
2016-02-13 20:24:53 +0530

    updated

--- circle-area.js
+++ circle-area.js
@@ -1,1 +1,5 @@
-
+function CalculateArea(){
+        var radius =document.form1.txtRadius.value;
+    	document.write("<p>The r is  " + radius + "</br></p");
+    	document.write("<P>The area of the circle is   " + (radius * radius * Math.PI) + "</p>");
+    }
